67164d2db1309a4ae80024f7d6b4283bc7ef9374
benaissa-saida <benaissasaida28@gmail.com>
2022-08-22 16:19:04 +0200

    tests bouclés

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom";
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
 import userEvent from "@testing-library/user-event";
 import { ROUTES_PATH, ROUTES } from "../constants/routes.js";
 import mockStore from "../__mocks__/store";
@@ -60,10 +60,10 @@ describe("Given I am connected as an employee", () => {
       root.setAttribute("id", "root");
       document.body.append(root);
       router();
-    })
+    });
 
     describe("When I choose a good extension (jpeg, jpg, png) file to upload", () => {
-      test('Then the file will pass and the name will be found with good extension', async () => {
+      test("Then the file will pass and the name will be found with good extension", async () => {
         const onNavigate = (pathname) => {
           document.body.innerHTML = ROUTES({ pathname });
         };
@@ -91,11 +91,11 @@ describe("Given I am connected as an employee", () => {
         inputFile.addEventListener("change", handleChangeFile);
 
         //simule le choix d'un fichier
-        await userEvent.upload(inputFile, file)
+        await userEvent.upload(inputFile, file);
 
+        expect(inputFile.files[0].name).toBe("image.jpg");
         expect(handleChangeFile).toBeCalled();
         expect(newBill.validFile).toBeTruthy();
-        expect(inputFile.files[0].name).toBe("image.jpg");
       });
     });
     describe("When I choose a wrong extension file to upload", () => {
@@ -130,9 +130,9 @@ describe("Given I am connected as an employee", () => {
         //simule le choix d'un fichier
         await userEvent.upload(inputFile, file);
 
-        expect(handleChangeFile).toBeCalled();
-        expect(newBill.validFile).not.toBeTruthy();
         expect(inputFile.files[0].name).not.toBe("test.png");
+        expect(handleChangeFile).toBeCalled();
+        expect(newBill.validFile).toBeFalsy();
         expect(
           screen.getByTestId("error-file-extension").classList
         ).toBeTruthy();
@@ -140,12 +140,101 @@ describe("Given I am connected as an employee", () => {
     });
   });
 
+  // test d'intégration POST
   describe('When I am on NewBills page and I click on button "Envoyer"', () => {
-    describe("When I fill form in correct format", () => {
-      test("Then an HTML error should appears", () => {});
+    beforeEach(() => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
     });
-    describe("When I fill form in incorrect format", () => {
-      test("Then I should create a NewBill", () => {});
+    describe("When I fill form in correct format", () => {
+      test("Then I should create a NewBill", async () => {
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname });
+        };
+
+        const html = NewBillUI();
+        document.body.innerHTML = html;
+
+        //initie le store
+        const store = mockStore;
+
+        //Initie newBill
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          store,
+          localStorage: window.localStorage,
+        });
+
+        // Define inputs values
+        const inputData = {
+          type: "Transports",
+          name: "Voyage Paris Nice",
+          amount: "174",
+          date: "2022-08-22",
+          vat: 70,
+          pct: 20,
+          file: new File(["img"], "image.png", { type: "image/png" }),
+          commentary: "dummy test",
+          status: "pending",
+        };
+
+        // Load the values in fields
+        screen.getByTestId("expense-type").value = inputData.type;
+        screen.getByTestId("expense-name").value = inputData.name;
+        screen.getByTestId("amount").value = inputData.amount;
+        screen.getByTestId("datepicker").value = inputData.date;
+        screen.getByTestId("vat").value = inputData.vat;
+        screen.getByTestId("pct").value = inputData.pct;
+        screen.getByTestId("commentary").value = inputData.commentary;
+
+        const inputFile = screen.getByTestId("file");
+
+        const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
+        const form = screen.getByTestId("form-new-bill");
+        form.addEventListener("submit", handleSubmit);
+
+        await userEvent.upload(inputFile, inputData.file);
+
+        fireEvent.submit(form);
+
+        expect(handleSubmit).toHaveBeenCalled();
+      });
+
+      test("Then it should render Bills page", () => {
+        expect(screen.getByTestId("btn-new-bill")).toBeTruthy();
+        expect(screen.getByText("Mes notes de frais")).toBeTruthy();
+      });
+      
+      describe("When an error occurs on API", () => {
+        test("Then it should fetches messages from an API and fails with 500 message error", async () => {
+          jest.spyOn(mockStore, "bills");
+          mockStore.bills.mockImplementationOnce(() => {
+            return {
+              list: () => {
+                return Promise.reject(new Error("Erreur 500"));
+              },
+            };
+          });
+
+          window.onNavigate(ROUTES_PATH.Bills);
+          await new Promise(process.nextTick);
+          const message = await screen.getByText(/Erreur 500/);
+          expect(message).toBeTruthy();
+        });
+      });
     });
   });
 });
